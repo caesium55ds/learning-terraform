c4962e9114afdd2c844fcabad5c1708c71b7ca81
Damien Suggs <43476799+caesium55ds@users.noreply.github.com>
2024-04-02 14:52:47 -0400

    autoscaling fix #3

--- main.tf
+++ main.tf
@@ -31,14 +31,14 @@ module "blog_vpc" {
 
 
 
-module "autoscaling" {
+module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "6.5.2"
 
   name = "blog"
+  
   min_size = 1
   max_size = 2
-
   vpc_zone_identifier = module.blog_vpc.public_subnets
   target_group_arns   = module.blog_alb.target_group_arns
   security_groups = [module.blog_sg.security_group_id]
